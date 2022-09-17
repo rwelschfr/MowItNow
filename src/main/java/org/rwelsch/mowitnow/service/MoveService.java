90809b507d208cf5fbfd628fcd12fc05cc772f87
Robin Welsch <rwelsch@orange.fr>
2022-09-17 20:18:21 +0200

    Code format

--- src/main/java/org/rwelsch/mowitnow/service/MoveService.java
+++ src/main/java/org/rwelsch/mowitnow/service/MoveService.java
@@ -5,7 +5,7 @@ import org.rwelsch.mowitnow.model.*;
 public class MoveService {
 
     public void move(Mower mower, Grid grid, Input input) {
-        switch(input) {
+        switch (input) {
             case A -> goForward(mower, grid);
             case D -> rotateRight(mower);
             case G -> rotateLeft(mower);
@@ -13,7 +13,7 @@ public class MoveService {
     }
 
     private void goForward(Mower mower, Grid grid) {
-        switch(mower.getOrientation()) {
+        switch (mower.getOrientation()) {
             case N -> goNorth(mower, grid.getTopRightCornerCoordinates().y());
             case E -> goEast(mower, grid.getTopRightCornerCoordinates().x());
             case W -> goWest(mower);
@@ -22,31 +22,31 @@ public class MoveService {
     }
 
     private void goNorth(Mower mower, int maxY) {
-        if(mower.getCoordinates().y() < maxY) {
+        if (mower.getCoordinates().y() < maxY) {
             mower.setCoordinates(new Coordinates(mower.getCoordinates().x(), mower.getCoordinates().y() + 1));
         }
     }
 
     private void goEast(Mower mower, int maxX) {
-        if(mower.getCoordinates().x() < maxX) {
+        if (mower.getCoordinates().x() < maxX) {
             mower.setCoordinates(new Coordinates(mower.getCoordinates().x() + 1, mower.getCoordinates().y()));
         }
     }
 
     private void goWest(Mower mower) {
-        if(mower.getCoordinates().x() > 0) {
+        if (mower.getCoordinates().x() > 0) {
             mower.setCoordinates(new Coordinates(mower.getCoordinates().x() - 1, mower.getCoordinates().y()));
         }
     }
 
     private void goSouth(Mower mower) {
-        if(mower.getCoordinates().y() > 0) {
+        if (mower.getCoordinates().y() > 0) {
             mower.setCoordinates(new Coordinates(mower.getCoordinates().x(), mower.getCoordinates().y() - 1));
         }
     }
 
     private void rotateRight(Mower mower) {
-        switch(mower.getOrientation()) {
+        switch (mower.getOrientation()) {
             case N -> mower.setOrientation(Orientation.E);
             case E -> mower.setOrientation(Orientation.S);
             case W -> mower.setOrientation(Orientation.N);
@@ -55,7 +55,7 @@ public class MoveService {
     }
 
     private void rotateLeft(Mower mower) {
-        switch(mower.getOrientation()) {
+        switch (mower.getOrientation()) {
             case N -> mower.setOrientation(Orientation.W);
             case E -> mower.setOrientation(Orientation.N);
             case W -> mower.setOrientation(Orientation.S);
